faa15e3b0d2102cbf74893dd0d42c0005a143004
Áine T. Dineen <51408125+dineenai@users.noreply.github.com>
2021-02-02 09:52:14 +0000

    Create train_supervised_resnet50.sh

---
+++ train_supervised_resnet50.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+#
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=12
+#SBATCH -J blurry_train_supervised_ResNet50_elephantcat
+#SBATCH --output=/home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/supervised_resnet50/logs/slurm-%j.out
+#SBATCH --error=/home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/supervised_resnet50/logs/slurm-%j.err
+
+
+DIR='/data/ILSVRC2012/blurry_vision/'
+PYTHON="/opt/anaconda3/envs/blurry_vision/bin/python"
+MODEL='resnet50'
+
+
+${PYTHON} main.py --a ${MODEL} \
+${DIR}
