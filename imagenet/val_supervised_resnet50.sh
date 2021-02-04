0d43bfa27f2e353b96501475bac5d520104d2c8b
Áine T. Dineen <51408125+dineenai@users.noreply.github.com>
2021-02-04 14:33:23 +0000

    Create val_supervised_resnet50.sh

---
+++ imagenet/val_supervised_resnet50.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=12
+#SBATCH -J blurry_val_supervised_ResNet50_elephantcat
+#SBATCH --output=/home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/supervised_resnet50/evaluate/logs/slurm-%j.out
+#SBATCH --error=/home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/supervised_resnet50/evaluate/logs/slurm-%j.err
+
+
+DIR='/data/ILSVRC2012/'
+PYTHON="/opt/anaconda3/envs/blurry_vision/bin/python"
+MODEL='resnet50'
+RESUME="/home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/ model_best.pth.tar"
+
+
+${PYTHON} main.py --a ${MODEL} \
+--resume ${RESUME} \
+--evaluate \
+${DIR}
+
