0cf4154f9baf24d9f7f0560d97121be42b1fe307
dineenai <ainedineen@gmail.com>
2021-03-16 17:33:01 +0000

    Test Results - Style Transfer

--- imagenet/test_results/test_results_sup_RN50_conv1_21_gauss_0_for_60_epoch60/top1_correct/cp_val_images_tex.sh
+++ imagenet/test_results/test_results_sup_RN50_conv1_21_gauss_0_for_60_epoch60/top1_correct/cp_val_images_tex.sh
@@ -1,5 +1,5 @@
 while read -r line
 do
     echo "$line"
-    cp /home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/val_images_tex/starfsh/val/images/$line  /home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/correctly_identified_val_images_tex_style_transfer/sup_RN50_conv1_21_gauss_0_for_60_epoch60/starfsh/$line --recursive
-done < /home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/test_results/test_results_sup_RN50_conv1_21_gauss_0_for_60_epoch60/top1_correct/top1_correct_sup_RN50_conv1_21_gauss_0_for_60_epoch60_starfsh.txt  
+    cp /home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/val_images_tex/ly_bfly/val/images/$line  /home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/correctly_identified_val_images_tex_style_transfer/sup_RN50_conv1_21_gauss_0_for_60_epoch60/ly_bfly/$line --recursive
+done < /home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/test_results/test_results_sup_RN50_conv1_21_gauss_0_for_60_epoch60/top1_correct/top1_correct_sup_RN50_conv1_21_gauss_0_for_60_epoch60_ly_bfly.txt  
--- imagenet/test_results/test_results_sup_RN50_from_gauss_4_for_30_epoch_to_gauss_0_for_30_epoch60/top1_correct/cp_afr_ele.sh
+++ imagenet/test_results/test_results_sup_RN50_from_gauss_4_for_30_epoch_to_gauss_0_for_30_epoch60/top1_correct/cp_afr_ele.sh
@@ -1,5 +1,5 @@
 while read -r line
 do
     echo "$line"
-    cp /home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/val_images_tex/gw_shrk/val/images/$line  /home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/correctly_identified_val_images_tex_style_transfer/sup_RN50_from_gauss_4_for_30_epoch_to_gauss_0_for_30_epoch60/gw_shrk/$line --recursive
-done < /home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/test_results/test_results_sup_RN50_from_gauss_4_for_30_epoch_to_gauss_0_for_30_epoch60/top1_correct/top1_correct_sup_RN50_from_gauss_4_for_30_epoch_to_gauss_0_for_30_epoch60_gw_shrk.txt  
+    cp /home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/val_images_tex/ly_bfly/val/images/$line  /home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/correctly_identified_val_images_tex_style_transfer/sup_RN50_from_gauss_4_for_30_epoch_to_gauss_0_for_30_epoch60/ly_bfly/$line --recursive
+done < /home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/test_results/test_results_sup_RN50_from_gauss_4_for_30_epoch_to_gauss_0_for_30_epoch60/top1_correct/top1_correct_sup_RN50_from_gauss_4_for_30_epoch_to_gauss_0_for_30_epoch60_ly_bfly.txt  
