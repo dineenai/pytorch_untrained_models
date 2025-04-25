d5682462f61904c275cd4d046b88118fdc535d95
dineenai <ainedineen@gmail.com>
2025-04-25 12:31:45 +0100

    Bandpass Training Accuracy

--- imagenet/train_supervised_resnet50_butterworth_low_save_batches_log.sh
+++ imagenet/train_supervised_resnet50_butterworth_low_save_batches_log.sh
@@ -3,13 +3,13 @@
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=12
 #SBATCH -J low_train_log
-#SBATCH --output=/data2/blurry_vision_sup_RN50/supervised_resnet50_bp_butter_low_batches_log_for_60_epoch_iter-1/logs/slurm-%j.out
-#SBATCH --error=/data2/blurry_vision_sup_RN50/supervised_resnet50_bp_butter_low_batches_log_for_60_epoch_iter-1/logs/slurm-%j.err
+#SBATCH --output=/data2/blurry_vision_sup_RN50/supervised_resnet50_bp_butter_low_batches_log_for_60_epoch_iter-10/logs/slurm-%j.out
+#SBATCH --error=/data2/blurry_vision_sup_RN50/supervised_resnet50_bp_butter_low_batches_log_for_60_epoch_iter-10/logs/slurm-%j.err
 
 
 
 # ITER=5
-ITER=1
+ITER=10
 BAND='low'
 OUTPTH="/data2/blurry_vision_sup_RN50/supervised_resnet50_bp_butter_${BAND}_batches_log_for_60_epoch_iter-${ITER}"
 
@@ -22,12 +22,13 @@ OUTFOLDER="${OUTPTH}/outmodel"
 PYTHON="/opt/anaconda3/envs/blurry_vision/bin/python"
 MODEL='resnet50'
 CPKT="supervised_resnet50_bp_butter_${BAND}_batches_for_60_epoch"
-EPOCHS=90
+# EPOCHS=90
+EPOCHS=3
 SAVE=1
 ACCPATH="/home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/bandpass_analysis_butterworth/batched_accuracy"
 PRINTFREQ=100
 BATCHNOPATH="/home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/log_batches.csv"
-RESUME="${OUTFOLDER}/checkpoint_supervised_resnet50_bp_butter_low_batches_for_60_epoch_epoch59_complete_log_iter-1.pth.tar" #Accidentally re-ran all!
+# RESUME="${OUTFOLDER}/checkpoint_supervised_resnet50_bp_butter_low_batches_for_60_epoch_epoch59_complete_log_iter-1.pth.tar" #Accidentally re-ran all!
 
 
 ${PYTHON} main_general_copy_mmk_pared_back_save_in_epoch_save_log_batches.py --a ${MODEL} \
@@ -39,5 +40,4 @@ ${PYTHON} main_general_copy_mmk_pared_back_save_in_epoch_save_log_batches.py --a
 --path_acc ${ACCPATH} \
 --iter ${ITER} \
 --batches_to_save ${BATCHNOPATH} \
---resume ${RESUME} \
 ${DIR}
--- imagenet/train_supervised_resnet50_butterworth_mid_save_batches_log.sh
+++ imagenet/train_supervised_resnet50_butterworth_mid_save_batches_log.sh
@@ -3,13 +3,13 @@
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=12
 #SBATCH -J mid_train_log
-#SBATCH --output=/data2/blurry_vision_sup_RN50/supervised_resnet50_bp_butter_mid_batches_log_for_60_epoch_iter-1/logs/slurm-%j.out
-#SBATCH --error=/data2/blurry_vision_sup_RN50/supervised_resnet50_bp_butter_mid_batches_log_for_60_epoch_iter-1/logs/slurm-%j.err
+#SBATCH --output=/data2/blurry_vision_sup_RN50/supervised_resnet50_bp_butter_mid_batches_log_for_60_epoch_iter-10/logs/slurm-%j.out
+#SBATCH --error=/data2/blurry_vision_sup_RN50/supervised_resnet50_bp_butter_mid_batches_log_for_60_epoch_iter-10/logs/slurm-%j.err
 
 
 
-# ITER=5
-ITER=1
+ITER=10
+# ITER=1
 BAND='mid'
 OUTPTH="/data2/blurry_vision_sup_RN50/supervised_resnet50_bp_butter_${BAND}_batches_log_for_60_epoch_iter-${ITER}"
 
@@ -22,12 +22,13 @@ OUTFOLDER="${OUTPTH}/outmodel"
 PYTHON="/opt/anaconda3/envs/blurry_vision/bin/python"
 MODEL='resnet50'
 CPKT="supervised_resnet50_bp_butter_${BAND}_batches_for_60_epoch"
-EPOCHS=60
+# EPOCHS=60
+EPOCHS=3
 SAVE=1
 ACCPATH="/home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/bandpass_analysis_butterworth/batched_accuracy"
 PRINTFREQ=100
 BATCHNOPATH="/home/ainedineen/blurry_vision/pytorch_untrained_models/imagenet/log_batches.csv"
-RESUME="${OUTFOLDER}/checkpoint_supervised_resnet50_bp_butter_mid_batches_for_60_epoch_epoch2_complete_log_iter-1.pth.tar"
+# RESUME="${OUTFOLDER}/checkpoint_supervised_resnet50_bp_butter_mid_batches_for_60_epoch_epoch2_complete_log_iter-1.pth.tar"
 
 ${PYTHON} main_general_copy_mmk_pared_back_save_in_epoch_save_log_batches.py --a ${MODEL} \
 --model_path ${OUTFOLDER} \
@@ -38,5 +39,4 @@ ${PYTHON} main_general_copy_mmk_pared_back_save_in_epoch_save_log_batches.py --a
 --path_acc ${ACCPATH} \
 --iter ${ITER} \
 --batches_to_save ${BATCHNOPATH} \
---resume ${RESUME} \
 ${DIR}
